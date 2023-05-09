60537cdb9b6bb878dacf7df518f9cc5b6b2c9430
Matt Payne <Payne@MattPayne.org>
2023-05-09 14:50:35 -0500

    Added schedule data and started testing methods that slice it.

---
+++ src/__tests__/Slices.test.tsx
@@ -0,0 +1,15 @@
+
+import Schedule from '../Schedule';
+
+describe('Slices meeting schedule data correctly', () => {
+    test('There are 23 people in the schedule', () => {
+        const schedule = Schedule();
+        expect(schedule.length).toBe(23);
+    });
+
+    test('2 plus 2 to be 4', () => {
+        expect(2 + 2).toBe(4);
+    });
+
+});
+
